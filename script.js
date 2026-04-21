80f2f0cb37cad3f475d2385410a0143bbc34d8a9
Jericho <valdezjericho321@gmail.com>
2026-04-21 22:31:13 +0800

    update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<script>
+
 const cartIcon = document.querySelector(".cart-icon");
 const cartDrawer = document.getElementById("cartDrawer");
 const closeCart = document.getElementById("closeCart");
@@ -11,18 +10,3 @@ cartIcon.addEventListener("click", () => {
 closeCart.addEventListener("click", () => {
     cartDrawer.classList.remove("active");
 });
-=======
-<script>
-const cartIcon = document.querySelector(".cart-icon");
-const cartDrawer = document.getElementById("cartDrawer");
-const closeCart = document.getElementById("closeCart");
-
-cartIcon.addEventListener("click", () => {
-    cartDrawer.classList.add("active");
-});
-
-closeCart.addEventListener("click", () => {
-    cartDrawer.classList.remove("active");
-});
->>>>>>> ebfa88542db5b9665f91098a57f6db0c7f62759e
-</script>
